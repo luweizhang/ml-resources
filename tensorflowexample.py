407ebe6971421de61675aa150b51f9b03be2c6c1
juicygranpa <luwzhang@gmail.com>
2017-11-29 01:22:45 -0800

    Rename tensorflow_example.py to tensorflowexample.py

---
+++ tensorflowexample.py
@@ -0,0 +1,62 @@
+#some simple tensorflow examples
+
+def linear_function():
+    """
+    Implements a linear function: 
+            Initializes W to be a random tensor of shape (4,3)
+            Initializes X to be a random tensor of shape (3,1)
+            Initializes b to be a random tensor of shape (4,1)
+    Returns: 
+    result -- runs the session for Y = WX + b 
+    """
+    
+    np.random.seed(1)
+    
+    ### START CODE HERE ### (4 lines of code)
+    X = tf.constant(np.random.randn(3,1), name = "X")
+    W = tf.constant(np.random.randn(4,3), name = "X")
+    b = tf.constant(np.random.randn(4,1), name = "X")
+    Y = tf.add(tf.matmul(W, X), b)
+    ### END CODE HERE ### 
+    
+    # Create the session using tf.Session() and run it with sess.run(...) on the variable you want to calculate
+    
+    ### START CODE HERE ###
+    sess = tf.Session()
+    result = sess.run(Y)
+    ### END CODE HERE ### 
+    
+    # close the session 
+    sess.close()
+
+    return result
+    
+    
+def sigmoid(z):
+    """
+    Computes the sigmoid of z
+    
+    Arguments:
+    z -- input value, scalar or vector
+    
+    Returns: 
+    results -- the sigmoid of z
+    """
+    
+    ### START CODE HERE ### ( approx. 4 lines of code)
+    # Create a placeholder for x. Name it 'x'.
+    x = tf.placeholder(tf.float32, name = "x")
+
+    # compute sigmoid(x)
+    sigmoid = tf.sigmoid(x)
+
+    # Create a session, and run it. Please use the method 2 explained above. 
+    # You should use a feed_dict to pass z's value to x. 
+    with tf.Session() as sess: 
+        # run the variables initialization (if needed), run the operations
+        result = sess.run(sigmoid, feed_dict = {x: z})
+        # This takes care of closing the session for you :)
+    
+    ### END CODE HERE ###
+    
+    return result
