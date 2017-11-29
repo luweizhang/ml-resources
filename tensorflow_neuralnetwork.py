48845f3b0368ff8f02b0ab96ca3ed210ca8f529a
juicygranpa <luwzhang@gmail.com>
2017-11-29 02:06:24 -0800

    Update tensorflow_neuralnetwork.py

--- tensorflow_neuralnetwork.py
+++ tensorflow_neuralnetwork.py
@@ -1,1 +1,25 @@
 #a deep neural network implemented in tensorflow
+
+def create_placeholders(n_x, n_y):
+    """
+    Creates the placeholders for the tensorflow session.
+    
+    Arguments:
+    n_x -- scalar, size of an image vector (num_px * num_px = 64 * 64 * 3 = 12288)
+    n_y -- scalar, number of classes (from 0 to 5, so -> 6)
+    
+    Returns:
+    X -- placeholder for the data input, of shape [n_x, None] and dtype "float"
+    Y -- placeholder for the input labels, of shape [n_y, None] and dtype "float"
+    
+    Tips:
+    - You will use None because it let's us be flexible on the number of examples you will for the placeholders.
+      In fact, the number of examples during test/train is different.
+    """
+
+    ### START CODE HERE ### (approx. 2 lines)
+    X = tf.placeholder(tf.float32, [n_x, None], name="X")
+    Y = tf.placeholder(tf.float32, [n_y, None], name="Y")
+    ### END CODE HERE ###
+    
+    return X, Y
