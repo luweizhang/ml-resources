bd0e10c05ed856ee02ce84b417f45e003da893e1
juicygranpa <luwzhang@gmail.com>
2017-11-29 02:11:55 -0800

    Update tensorflow_neuralnetwork.py

--- tensorflow_neuralnetwork.py
+++ tensorflow_neuralnetwork.py
@@ -23,3 +23,39 @@ def create_placeholders(n_x, n_y):
     ### END CODE HERE ###
     
     return X, Y
+
+
+# GRADED FUNCTION: initialize_parameters
+def initialize_parameters():
+    """
+    Initializes parameters to build a neural network with tensorflow. The shapes are:
+                        W1 : [25, 12288]
+                        b1 : [25, 1]
+                        W2 : [12, 25]
+                        b2 : [12, 1]
+                        W3 : [6, 12]
+                        b3 : [6, 1]
+    
+    Returns:
+    parameters -- a dictionary of tensors containing W1, b1, W2, b2, W3, b3
+    """
+    
+    tf.set_random_seed(1)                   # so that your "random" numbers match ours
+        
+    ### START CODE HERE ### (approx. 6 lines of code)
+    W1 = tf.get_variable("W1", [25, 12288], initializer = tf.contrib.layers.xavier_initializer(seed=1))
+    b1 = tf.get_variable("b1", [25, 1], initializer = tf.zeros_initializer())
+    W2 = tf.get_variable("W2", [12, 25], initializer = tf.contrib.layers.xavier_initializer(seed=1))
+    b2 = tf.get_variable("b2", [12, 1], initializer = tf.zeros_initializer())
+    W3 = tf.get_variable("W3", [6, 12], initializer = tf.contrib.layers.xavier_initializer(seed=1))
+    b3 = tf.get_variable("b3", [6, 1], initializer = tf.zeros_initializer())
+    ### END CODE HERE ###
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2,
+                  "W3": W3,
+                  "b3": b3}
+    
+    return parameters
