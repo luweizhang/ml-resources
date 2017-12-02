51459920d98dcda0bcb2c4f5619b087e72a244ec
luwei zhang <luwzhang@gmail.com>
2017-12-01 17:58:57 -0800

    Update tensorflow_convnet.py

--- tensorflow_convnet.py
+++ tensorflow_convnet.py
@@ -1,1 +1,26 @@
 #basic conv net using tensorflow
+
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import ndimage
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from cnn_utils import *
+
+%matplotlib inline
+
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+Y_train = convert_to_one_hot(Y_train_orig, 6).T
+Y_test = convert_to_one_hot(Y_test_orig, 6).T
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+conv_layers = {}
